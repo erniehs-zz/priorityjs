8973a31e59bbc001bff3ca8b6d5a15595e1f18cf
ernie <ernie_hs@hotmail.com>
2016-03-18 10:30:34 +0000

    more tests and implementations etc.

--- src/priorityq.js
+++ src/priorityq.js
@@ -1,21 +1,29 @@
-function PriorityQ() {
+function PriorityQ(comp) {
+	if (!comp) {
+		throw 'comparator function required';
+	}
+	this.comp = comp;
 	this.items = [];
 }
 
 PriorityQ.prototype.push = function(item) {
-	this.items.push(item);
+	throw 'not implemented yet';
 };
 
 PriorityQ.prototype.pop = function() {
-	return this.items.shift();
+	return this.items.pop();
 };
 
 PriorityQ.prototype.peek = function() {
-	return this.items.peek();
+	return this.items[0];
 };
 
 PriorityQ.prototype.size = function() {
 	return this.items.length;
+};
+
+PriorityQ.prototype.clear = function() {
+	this.items.clear();
 };
 
 PriorityQ.prototype.isEmpty = function() {
